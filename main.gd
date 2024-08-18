b549684a9bffbf5d8e68a3c86800ada6104ff5ea
Francesco Michelini <hello@francescomichelini.com>
2024-08-18 13:34:40 +0200

    Spawn mobs

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+func _on_mob_timer_timeout() -> void:
+  var mob = mob_scene.instantiate()
+
+  var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+  mob_spawn_location.progress_ratio = randf()
+
+  mob.initialize(mob_spawn_location.position, $Player.position)
+
+  add_child(mob)
