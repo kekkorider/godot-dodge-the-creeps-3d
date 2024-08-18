9cd13204dff13fcc54752ead4c1d79e9095aec13
Francesco Michelini <hello@francescomichelini.com>
2024-08-18 15:14:16 +0200

    Add score, replay and music

--- main.gd
+++ main.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready() -> void:
+  $UserInterface/Retry.hide()
+
 func _on_mob_timer_timeout() -> void:
   var mob = mob_scene.instantiate()
 
@@ -10,8 +13,15 @@ func _on_mob_timer_timeout() -> void:
 
   mob.initialize(mob_spawn_location.position, $Player.position)
 
+  mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
+
   add_child(mob)
 
 
 func _on_player_hit() -> void:
+  $UserInterface/Retry.show()
   $MobTimer.stop()
+
+func _unhandled_input(event: InputEvent) -> void:
+  if event.is_action_pressed("ui_accept") && $UserInterface/Retry.visible:
+    get_tree().reload_current_scene()
