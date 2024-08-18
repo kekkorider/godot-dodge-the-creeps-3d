f02ac45984c2b11a30d3c56862b9695d80a68c93
Francesco Michelini <hello@francescomichelini.com>
2024-08-18 12:58:10 +0200

    Add mobs

---
+++ mob.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+@export var min_speed: int = 10
+@export var max_speed: int = 20
+
+func _physics_process(_delta) -> void:
+  move_and_slide()
+
+func initialize(start_position: Vector3, player_position: Vector3) -> void:
+  # Place the mob at `start_position` and rotate it
+  # towards `player_position`
+  look_at_from_position(start_position, player_position, Vector3.UP)
+
+  # Randomly rotate the mob so that it doesn't
+  # look directly at the player
+  rotate_y(randf_range(-PI / 4, PI / 4))
+
+  # Calculate a random range
+  var random_speed: int = randi_range(min_speed, max_speed)
+
+  # Calculate a forward velocity thet represents the speed
+  velocity = Vector3.FORWARD * random_speed
+
+  # We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking.
+  velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+  queue_free()
